56ef5f1f222d906c6272b08c0cc9ad69463d6c92
Jorge Costafreda <jorge.costafreda@yahoo.com>
2020-11-05 15:49:08 -0500

    Worked on 978 G

---
+++ P978/GPetyasExams.java
@@ -0,0 +1,9 @@
+package P978;
+
+import java.util.Scanner;
+import java.io.PrintWriter;
+
+public class GPetyasExams {
+    public void solve(int testNumber, Scanner in, PrintWriter out) {
+    }
+}
